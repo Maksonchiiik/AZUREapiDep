462d75cf380a25c644818e13eb6e2c23d7ad43f1
Max <max@gmail.com>
2024-12-08 20:01:49 +0200

    update

--- event_platform/deployment.py
+++ event_platform/deployment.py
@@ -8,14 +8,13 @@ DEBUG = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
@@ -33,6 +32,4 @@ DATABASES = {
         'USER':  parameters['user'],
         'PASSWORD': parameters['password'],
     }
-
-
 }
